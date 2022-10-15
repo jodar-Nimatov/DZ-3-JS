d2547dc42dc1afa422d75dfc5c3a7531a7001c71
jodar <zodarbekn@gmail.com>
2022-10-15 19:26:53 +0300

    dz-3-js

---
+++ script.js
@@ -0,0 +1,35 @@
+// const num1 = prompt("enter the first number")
+// const num2 = prompt("enter the second number") 
+
+// function count (num1, num2) {
+//     if (num1 < num2) {
+//         alert(num1 + " less than " + num2)
+//     }else if (num1 > num2) {
+//         alert(num1 + " more than " + num2)
+//     }else {
+//         alert("equals")
+//     }
+// }
+// count(num1, num2)
+
+//-------2-------//
+
+// let array1 = [1,2,3,4]
+// let array2 = [1,2,3,4,5]
+
+// function TwoMass (array1, array2) {
+//     if (array1.length < array2.length) {
+//         console.log(array2.length)
+//     } else {
+//         console.log(array1.length);
+//     }
+// }
+// TwoMass (array1, array2)
+
+//-------3------//
+
+// const str = prompt("enter the word")
+// function CountChar (str) {
+//     console.log(str.length);
+// }
+// CountChar (str)
